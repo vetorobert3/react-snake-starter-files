3da1c110b2f169f4484fa879223ff63a58c0189f
Veto Ramirez <vetorobert3@gmail.com>
2019-12-28 13:34:02 -0800

    Created the border for the game, the snake, and the apple.

--- react-snake-START-HERE/src/App.js
+++ react-snake-START-HERE/src/App.js
@@ -1,5 +1,73 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 
-const App = () => <div>Snake Game</div>
+import { useInterval } from './useInterval';
+import {
+  CANVAS_SIZE,
+  SNAKE_START,
+  APPLE_START,
+  SCALE,
+  SPEED,
+  DIRECTIONS
+} from './constants';
+
+const App = () => {
+  const canvasRef = useRef(null);
+  const [snake, setSnake] = useState(SNAKE_START);
+  const [apple, setApple] = useState(APPLE_START);
+  const [dir, setDir] = useState([0, -1]);
+  const [speed, setSpeed] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+  
+  const startGame = () => {
+
+  }
+
+  const endGame = () => {
+
+  }
+
+  const moveSnake = () => {
+
+  }
+
+  const createApple = () =>{
+
+  }
+
+  const checkCollision = () => {
+
+  }
+
+  const checkAppleCollision = () => {
+
+  }
+
+  const gameLoop = () => {
+
+  }
+
+  useEffect(() => {
+    const context = canvasRef.current.getContext('2d');
+    context.setTransform(SCALE, 0, 0, SCALE, 0, 0);
+    context.clearRect(0, 0, CANVAS_SIZE[0], CANVAS_SIZE[1]);
+    context.fillStyle = 'lightgreen';
+    snake.forEach(([x, y]) => context.fillRect(x, y, 1, 1));
+    context.fillStyle = 'red';
+    context.fillRect(apple[0], apple[1], 1, 1);
+  }, [snake, apple, gameOver])
+
+  return(
+    <div role='button' tabIndex='0' onKeyDown={e => moveSnake}>
+      <canvas
+        style={{border: '1px solid black'}}
+        ref={canvasRef}
+        width={`${CANVAS_SIZE[0]}px`}
+        height={`${CANVAS_SIZE[1]}px`}
+      />
+      {gameOver && <div>GAME OVER!</div>}
+      <button onClick={startGame}>Start Game</button>
+    </div>
+  )
+}
   
 export default App;
