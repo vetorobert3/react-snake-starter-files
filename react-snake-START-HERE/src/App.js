8f471df18d2d61e1709b1bed1e621186951ff690
Veto Ramirez <vetorobert3@gmail.com>
2019-12-28 14:01:46 -0800

    Snake can now move on the screen.

--- react-snake-START-HERE/src/App.js
+++ react-snake-START-HERE/src/App.js
@@ -44,7 +44,7 @@ const App = () => {
   const gameLoop = () => {
     const snakeCopy = JSON.parse(JSON.stringify(snake));
     const newSnakeHead = [snakeCopy[0][0] + dir[0], snakeCopy[0][1] + dir[1]];
-    snakeCopy.unShift(newSnakeHead);
+    snakeCopy.unshift(newSnakeHead);
     snakeCopy.pop();
     setSnake(snakeCopy);
   }
@@ -59,8 +59,10 @@ const App = () => {
     context.fillRect(apple[0], apple[1], 1, 1);
   }, [snake, apple, gameOver])
 
+  useInterval(() => gameLoop(), speed);
+
   return(
-    <div role='button' tabIndex='0' onKeyDown={e => moveSnake}>
+    <div role='button' tabIndex='0' onKeyDown={e => moveSnake(e)}>
       <canvas
         style={{border: '1px solid black'}}
         ref={canvasRef}
