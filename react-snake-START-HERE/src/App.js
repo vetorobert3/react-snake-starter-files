a97a5f24e2558e72bcf69292411f72bb32605169
Veto Ramirez <vetorobert3@gmail.com>
2019-12-28 16:51:25 -0800

    Finished snake game.

--- react-snake-START-HERE/src/App.js
+++ react-snake-START-HERE/src/App.js
@@ -34,9 +34,8 @@ const App = () => {
   const moveSnake = ({ keyCode }) =>  
     keyCode >= 37 && keyCode <= 40 && setDir(DIRECTIONS[keyCode]);
 
-  const createApple = () =>{
-
-  }
+  const createApple = () =>
+    apple.map((_, i) => Math.floor(Math.random() * (CANVAS_SIZE[i]) / SCALE));
 
   const checkCollision = (piece, snk = snake) => {
     if (
@@ -52,8 +51,16 @@ const App = () => {
   return false;
   }
 
-  const checkAppleCollision = () => {
-
+  const checkAppleCollision = newSnake => {
+    if (newSnake[0][0] === apple[0] && newSnake[0][1] === apple[1]) {
+      let newApple = createApple();
+      while (checkCollision(newApple, newSnake)) {
+        newApple = createApple();
+      }
+      setApple(newApple);
+      return true;
+    }
+    return false;
   }
 
   const gameLoop = () => {
@@ -61,7 +68,7 @@ const App = () => {
     const newSnakeHead = [snakeCopy[0][0] + dir[0], snakeCopy[0][1] + dir[1]];
     snakeCopy.unshift(newSnakeHead);
     if (checkCollision(newSnakeHead)) endGame();
-    snakeCopy.pop();
+    if (!checkAppleCollision(snakeCopy)) snakeCopy.pop();
     setSnake(snakeCopy);
   }
 
